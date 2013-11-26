cf81d999b08f7951d487fb5615b441cbb8f06cf2
Shannon Lewis <slewis74@gmail.com>
2013-11-27 08:47:11 +1000

    Pinning now toggles

--- Jukebox/Jukebox/Features/Albums/PinAlbumCommand.cs
+++ Jukebox/Jukebox/Features/Albums/PinAlbumCommand.cs
@@ -1,14 +1,9 @@
 ﻿using System;
-using Windows.Foundation;
-using Windows.UI.StartScreen;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Media;
-using Slab.ViewModels;
+using SlabRt.ViewModels;
 
 namespace Jukebox.Features.Albums
 {
-    public class PinAlbumCommand : Command
+    public class PinAlbumCommand : TogglePinCommand
     {
         private readonly AlbumViewModel _albumViewModel;
 
@@ -18,70 +13,29 @@ namespace Jukebox.Features.Albums
             _albumViewModel = albumViewModel;
         }
 
-        public string AppbarTileId { get { return string.Format("SecondaryTile.AppBar.JukeboxAlbum.{0}.{1}", _albumViewModel.ArtistName.Replace(' ', '.'), _albumViewModel.Title.Replace(' ', '.')); } }
-
-        public async override void Execute(object parameter)
+        public override string AppbarTileId
         {
-            var button = (Button)parameter;
-
-            var parent = (FrameworkElement)button.Parent;
-            while ((parent is AppBar) == false)
+            get
             {
-                parent = (FrameworkElement)parent.Parent;
+                return string.Format("Album.{0}.{1}", 
+                    _albumViewModel.ArtistName.Replace(' ', '.'),
+                    _albumViewModel.Title.Replace(' ', '.').Replace(':', '.'));
             }
-            var appBar = (AppBar)parent;
+        }
 
-            appBar.IsSticky = true;
+        public override string TileTitle
+        {
+            get { return _albumViewModel.Title; }
+        }
 
-            if (SecondaryTile.Exists(AppbarTileId))
-            {
-                var secondaryTile = new SecondaryTile(AppbarTileId);
-                var isUnpinned = await secondaryTile.RequestDeleteForSelectionAsync(button.GetElementRect(), Windows.UI.Popups.Placement.Above);
+        public override string ActivationArguments
+        {
+            get { return "Album/ShowAlbum?artistName=" + _albumViewModel.ArtistName + "&albumTitle=" + _albumViewModel.Title; }
+        }
 
-                //ToggleAppBarButton(isUnpinned);
-            }
-            else
-            {
-                var logo = new Uri("ms-appx:///Assets/Logo.png");
-                var wideLogo = new Uri("ms-appx:///Assets/WideLogo.png");
-                var tileActivationArguments = "Album/ShowAlbum?artistName=" + _albumViewModel.ArtistName + "&albumTitle=" + _albumViewModel.Title;
-
-                var secondaryTile = new SecondaryTile(AppbarTileId,
-                                                        _albumViewModel.ArtistName,
-                                                        _albumViewModel.Title,
-                                                        tileActivationArguments,
-                                                        TileOptions.ShowNameOnWideLogo,
-                                                        logo,
-                                                        wideLogo)
-                {
-                    ForegroundText = ForegroundText.Dark,
-                    SmallLogo = new Uri("ms-appx:///Assets/SmallLogo.png")
-                };
-
-                bool isPinned = await secondaryTile.RequestCreateForSelectionAsync((button).GetElementRect(), Windows.UI.Popups.Placement.Above);
-
-                //ToggleAppBarButton(!isPinned);
-            }
-            appBar.IsSticky = false;
+        public override Uri TileImageUri
+        {
+            get { return new Uri("ms-appx:///Assets/Logo.png"); }
         }
     }
-
-    public static class Positioning
-    {
-        public static Rect GetElementRect(this FrameworkElement element, int hOffset, int vOffset)
-        {
-            var rect = GetElementRect(element);
-            rect.Y += vOffset;
-            rect.X += hOffset;
-            return rect;
-        }
-
-        public static Rect GetElementRect(this FrameworkElement element)
-        {
-            GeneralTransform buttonTransform = element.TransformToVisual(null);
-            Point point = buttonTransform.TransformPoint(new Point());
-            return new Rect(point, new Size(element.ActualWidth, element.ActualHeight));
-        }
-    }
-
 }
