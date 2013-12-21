3654d94ca796a34b5cf565c818e4ddc26f7b9df2
Shannon Lewis <slewis74@gmail.com>
2013-12-21 19:38:51 +1000

    slab updates for header control

--- Jukebox/Jukebox/Modules/ViewModelModule.cs
+++ Jukebox/Jukebox/Modules/ViewModelModule.cs
@@ -1,5 +1,6 @@
 ﻿using System.Reflection;
 using Autofac;
+using SlabRt.Commands;
 
 namespace Jukebox.Modules
 {
@@ -7,6 +8,8 @@ namespace Jukebox.Modules
     {
         protected override void Load(ContainerBuilder builder)
         {
+            builder.RegisterType<GoBackCommand>().AsSelf().InstancePerDependency();
+            
             builder
                 .RegisterAssemblyTypes(typeof (ViewModelModule).GetTypeInfo().Assembly)
                 .Where(t => t.Name.EndsWith("ViewModel"))
